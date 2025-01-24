71adc9a8a2a43b75094ac6d66542ec349cf1c65b
hugoguilhermemaestri <hugo.maestri@gmail.com>
2025-01-24 10:48:19 -0300

    feat: added examples 01 and 02

---
+++ exemplo_01.py
@@ -0,0 +1,6 @@
+# Exercício 01
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+nome = input("Digite seu nome: ")
+caracteres = len(nome)
+print("O nome", nome, "possui", caracteres, "caracteres")
---
+++ exemplo_02.py
@@ -0,0 +1,7 @@
+# Exercício 02
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+valor_1 = int(input("Digite o primeiro valor: "))
+valor_2 = int(input("Digite o segundo valor: "))
+soma = valor_1 + valor_2
+print("A soma de ", valor_1, "+", valor_2, "é igual a", soma)
