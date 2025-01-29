f65c0e5e9c6cba6937ac83e0e5113b0112c3cf2f
hugoguilhermemaestri <hugo.maestri@gmail.com>
2025-01-29 14:57:07 -0300

    :recycle: refactor: add function for each action and create a list of registered users

--- aula04/kpi04.py
+++ aula04/kpi04.py
@@ -2,51 +2,128 @@
 # o valor do seu salário mensal e o valor do bônus que recebeu. O programa deve, 
 # então, imprimir uma mensagem saudando o usuário pelo nome e informando o valor 
 # do salário em comparação com o bônus recebido.
+from typing import Dict, Any
 
 BONUS_FIXO = 1000
 
-nome_valido: bool = False
-salario_valido: bool = False
-bonus_valido: bool = False
+def cadastrar_nome_usuario() -> str:
+    nome_valido: bool = False    
+    while nome_valido is not True:
+        try:
+            nome: str = input("Digite seu nome: ")
+            if nome == "":
+                raise ValueError("Nome não pode estar em branco")
+            elif nome.isnumeric():
+                raise ValueError("Nome não pode ser numérico")
+            elif any(letra.isdigit() for letra in nome):
+                raise ValueError("Nome não pode conter números")
+            elif nome.isspace():
+                raise ValueError("Nome não pode ser apenas espaços")
+            else:
+                nome_valido = True
+        except ValueError as e:
+            print(e)
 
-while nome_valido is not True:
-    try:
-        nome: str = input("Digite seu nome: ")
-        if nome == "":
-            raise ValueError("Nome não pode estar em branco")
-        elif nome.isnumeric():
-            raise ValueError("Nome não pode ser numérico")
-        elif any(letra.isdigit() for letra in nome):
-            raise ValueError("Nome não pode conter números")
-        elif nome.isspace():
-            raise ValueError("Nome não pode ser apenas espaços")
-        else:
-            nome_valido = True
-    except ValueError as e:
-        print(e)
+    return nome
 
-while salario_valido is not True:
-    try:
-        salario: float = float(input("Digite o valor do seu salário mensal: "))
-        if salario < 0:
-            raise ValueError("Salário não pode ser negativo")
-        if salario == 0:
-            raise ValueError("Salário não pode ser zero")
-        else:
-            salario_valido = True
-    except ValueError as e:
-        print(e)
+def cadastrar_salario() -> float:
+    salario_valido: bool = False
+    while salario_valido is not True:
+        try:
+            salario: float = float(input("Digite o valor do seu salário mensal: "))
+            if salario < 0:
+                raise ValueError("Salário não pode ser negativo")
+            if salario == 0:
+                raise ValueError("Salário não pode ser zero")
+            else:
+                salario_valido = True
+        except ValueError as e:
+            print(e)
+    
+    return salario
 
-while bonus_valido is not True:
-    try:
-        bonus: float = float(input("Digite o valor do seu bonus mensal: "))
-        if bonus < 0:
-            raise ValueError("Bonus não pode ser negativo")
-        else:
-            bonus_valido = True
-    except ValueError as e:
-        print(e)
+def cadastrar_bonus() -> float:
+    bonus_valido: bool = False
+    while bonus_valido is not True:
+        try:
+            bonus: float = float(input("Digite o valor do seu bonus mensal: "))
+            if bonus < 0:
+                raise ValueError("Bonus não pode ser negativo")
+            else:
+                bonus_valido = True
+        except ValueError as e:
+            print(e)
+
+    return bonus
+
+def calcular_kpi_bonus(salario: float, bonus: float) -> float:
+    kpi_bonus: float = BONUS_FIXO + salario * bonus
+    
+    return kpi_bonus
+
+def cadastrar_usuario() -> Dict[str, Any]:
+    nome: str = cadastrar_nome_usuario()
+    salario: float = cadastrar_salario()
+    bonus: float = cadastrar_bonus()
+    bonus_recebido: float = calcular_kpi_bonus(salario=salario, bonus=bonus)
+
+    return {"nome": nome, "salario": salario, "bonus": bonus, "bonus_recebido": bonus_recebido}
+
+
+banco_usuarios: list[Dict[str, Any]] = []
+
+while True:
+    banco_usuarios.append(cadastrar_usuario())
+
+    continuar_cadastro = input("\nDeseja cadastrar outro usuário? (s/n): ")
+
+    if continuar_cadastro.lower() == "n":
+        break
+
+print(banco_usuarios)
+
+
+
+ ###############################################################       
+
+# while nome_valido is not True:
+#     try:
+#         nome: str = input("Digite seu nome: ")
+#         if nome == "":
+#             raise ValueError("Nome não pode estar em branco")
+#         elif nome.isnumeric():
+#             raise ValueError("Nome não pode ser numérico")
+#         elif any(letra.isdigit() for letra in nome):
+#             raise ValueError("Nome não pode conter números")
+#         elif nome.isspace():
+#             raise ValueError("Nome não pode ser apenas espaços")
+#         else:
+#             nome_valido = True
+#     except ValueError as e:
+#         print(e)
+
+# while salario_valido is not True:
+#     try:
+#         salario: float = float(input("Digite o valor do seu salário mensal: "))
+#         if salario < 0:
+#             raise ValueError("Salário não pode ser negativo")
+#         if salario == 0:
+#             raise ValueError("Salário não pode ser zero")
+#         else:
+#             salario_valido = True
+#     except ValueError as e:
+#         print(e)
+
+# while bonus_valido is not True:
+#     try:
+#         bonus: float = float(input("Digite o valor do seu bonus mensal: "))
+#         if bonus < 0:
+#             raise ValueError("Bonus não pode ser negativo")
+#         else:
+#             bonus_valido = True
+#     except ValueError as e:
+#         print(e)
         
-kpi_bonus: float = BONUS_FIXO + salario * bonus
+# kpi_bonus: float = BONUS_FIXO + salario * bonus
 
-print(f"Olá, {nome}. Seu salário é de R$ {salario:.2f} e o seu bônus recebido foi de R$ {kpi_bonus:.2f}")
+# print(f"Olá, {nome}. Seu salário é de R$ {salario:.2f} e o seu bônus recebido foi de R$ {kpi_bonus:.2f}")
